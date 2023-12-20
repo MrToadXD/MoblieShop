ece0ba4485fe08b63c07eb915e70332c1c9c2ef0
lam <lam@LAPTOP-IMNMLA1R>
2023-12-20 22:28:54 +0700

    Add project files.

---
+++ Ictshop/Controllers/SanphamController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ictshop.Models;
+
+namespace Ictshop.Controllers
+{
+    public class SanphamController : Controller
+    {
+        Qlbanhang db = new Qlbanhang();
+
+        // GET: Sanpham
+        public ActionResult dtiphonepartial()
+        {
+            var ip = db.Sanphams.Where(n=>n.Mahang==2).Take(4).ToList();
+           return PartialView(ip);
+        }
+        public ActionResult dtsamsungpartial()
+        {
+            var ss = db.Sanphams.Where(n => n.Mahang == 1).Take(4).ToList();
+            return PartialView(ss);
+        }
+        public ActionResult dtxiaomipartial()
+        {
+            var mi = db.Sanphams.Where(n => n.Mahang == 3).Take(4).ToList();
+            return PartialView(mi);
+        }
+        //public ActionResult dttheohang()
+        //{
+        //    var mi = db.Sanphams.Where(n => n.Mahang == 5).Take(4).ToList();
+        //    return PartialView(mi);
+        //}
+        public ActionResult xemchitiet(int Masp=0)
+        {
+            var chitiet = db.Sanphams.SingleOrDefault(n=>n.Masp==Masp);
+            if (chitiet == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(chitiet);
+        }
+
+        public ActionResult timkiem(string searchTerm)
+        {
+            // Kiểm tra xem searchTerm có tồn tại không
+            if (string.IsNullOrEmpty(searchTerm))
+            {
+                // Nếu không, trả về tất cả các dữ liệu (hoặc bạn có thể trả về một trang tìm kiếm trống)
+                var allData = db.Sanphams.ToList(); // Thay thế "YourModel" bằng tên thực tế của mô hình của bạn
+                return View(allData);
+            }
+
+            // Nếu có searchTerm, thực hiện tìm kiếm trong cơ sở dữ liệu
+            var searchResults = db.Sanphams.Where(x => x.Tensp.ToLower().Contains(searchTerm.ToLower())).ToList();
+
+            return View(searchResults);
+        }
+
+    }
+
+}
---
+++ Ictshop/Scripts/gui.js
@@ -0,0 +1,55 @@
+﻿//Các xử lí kịch bản cho index.html//
+
+//Khai báo biến 
+var def = '--- từ khóa ---';
+let emp = "";
+const ABC = '';
+
+//Concrete function >< Abstract function 
+function setFirstTime(){
+	window.document.frmSearch.txtKeyword.value = def;
+	//document.frmSearch.txtKeyword.value = def;
+	//window.document.form[0].txtKeyword.value = def;
+	//document.form[0].txtKeyword.value = def;
+	
+}
+
+function setFirstTimeV2(){
+	document.getElementById("txtKeyword").value = def;
+	//document.getElementById("frmSearch").txtKeyword.value = def;
+	let fn = document.getElementById("frmSearch");
+}
+
+function setFirstTimeV3(fn){
+	//Truyền tham chiếu đối tượng form
+	fn.txtKeyword.value = def;
+}
+
+function setKeyword(fn, isClick){
+	//Lấy giá trị 
+	let key = fn.txtKeyword.value;
+	
+	if(isClick){
+	if(key.trim()==def){
+		fn.txtKeyword.value = emp;
+	    }
+	}
+	else {
+		if(key.trim()==emp){
+			fn.txtKeyword.value = def;
+	}
+}
+}
+
+function checkValidKeyword(fn){
+	let key = fn.txtKeyword.value;
+
+	var message = 'Please input keyword for search';
+	
+if((key.trim()==def) || (key.trim() == emp)){
+	window.alert(message);
+	fn.txtKeyword.focus();
+	fn.txtKeyword.select();
+}
+			
+}
